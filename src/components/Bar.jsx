77b5ec5ed67bc96adf91e5ed5f8002ed214de0d5
Felix Mehta <28467515+felixm-pw@users.noreply.github.com>
2021-03-12 13:13:31 +0000

    Home button added + small changes

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 // Material Ui
 import {IconButton, AppBar, Typography, Toolbar} from '@material-ui/core'
 import SchoolIcon from '@material-ui/icons/School'
- 
+// External
+import { Redirect } from 'react-router-dom'
+
 class Bar extends React.Component {
     constructor(){
         super()
+        this.page_move = this.page_move.bind(this)
         this.state = { 
-
+            move: ""
         }
     }
 
@@ -17,14 +20,24 @@ class Bar extends React.Component {
             <div style={{margin: 0}}>
               <AppBar position="static" style={{width: "100%", backgroundColor: "#ffffff"}}>
                     <Toolbar>
-                    <IconButton edge="start" aria-label="menu">
+                    <IconButton edge="start" aria-label="menu" onClick={() => this.page_move()}>
                         <SchoolIcon/>
                     </IconButton>
                         <Typography variant="h6" style={{color: "#757575"}}>Lectern</Typography>
                     </Toolbar>
                 </AppBar>
+
+                {this.state.move}
+
             </div>
         )
     }
+
+    page_move(x){
+        this.setState({
+            move: <Redirect push to={{pathname: "/home"}} />
+        })
+    }
+
 }
 export default Bar
